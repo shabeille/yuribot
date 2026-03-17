c56d18a3490e8c74a69dcd96d10ecb8236e61b4e
shabeille <lolbitek2009@gmail.com>
2026-03-17 21:30:11 +0000

    Revert "added random offset to browser to avoid duplicate posts"
    This reverts commit 7ebef33bf3ded191a1e3d47766e1ea27ec5a417d.

--- safebooru.py
+++ safebooru.py
@@ -1,29 +1,18 @@
 from string import Template
 from urllib.parse import urljoin
-from random import choice, randint
-import xmltodict
+from random import choice
 
 POSTS_PER_PAGE = 42
 
 website_url: str = "https://safebooru.org/"
-
-post_prefix: Template = Template(
+prefix: Template = Template(
     "index.php?page=dapi&s=post&q=index&json=1"
     "&limit=${limit}"
     "&tags=${tags}"
-    "&pid=${offset}"
 )
-tag_prefix: Template = Template(
-    "index.php?page=dapi&s=tag&q=index"
-    "&name=${tag_name}"
-)
-
-
-
-    
 
 class SafebooruBrowser:
-    def __init__(self, session, cache_size=1000, default_tags=None, random_offset:bool=True):
+    def __init__(self, session, cache_size=1000, default_tags=None):
         self._session = session
 
         if default_tags is not None and type(default_tags) not in (tuple, list):
@@ -34,9 +23,6 @@ class SafebooruBrowser:
         self.cache_size = cache_size
         self._cached_posts = []
 
-        self.do_offset: bool = random_offset
-        self._offset: int = -1 # Set to -1 when no offset is present
-
     async def _get_json_response(self, url):
         async with self._session.get(url) as resp:
             return await resp.json()
@@ -46,20 +32,13 @@ class SafebooruBrowser:
 
         url = urljoin(
             website_url,
-            post_prefix.substitute(
+            prefix.substitute(
                 limit = self.cache_size,
-                tags = tags,
-                offset = 0 if self._offset == -1 else self._offset
+                tags = tags
             )
         )
 
         self._cached_posts = await self._get_json_response(url)
-
-        tag_count = await self.get_tag_count(self.default_tags[0])
-
-        if self.do_offset and tag_count >= self.cache_size:
-            self._offset = randint(0, tag_count - self.cache_size)
-            print(f"Offset of {self._offset}")
 
     async def get_random(self, *args):
         """
@@ -74,15 +53,3 @@ class SafebooruBrowser:
 
     async def get_cache_size(self):
         return len(self._cached_posts)
-
-    async def get_tag_count(self, tag: str) -> int:
-        url = urljoin(
-            website_url,
-            tag_prefix.substitute(
-                tag_name=tag
-            )
-        )
-
-        async with self._session.get(url) as resp:
-            tag_response = xmltodict.parse(await resp.text())
-            return int(tag_response["tags"]["tag"]["@count"]) # duct tape i think
